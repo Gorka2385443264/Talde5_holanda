36224ef5ce9ce3c8f29a8acdb04ca94aca25075d
Urko <urkolozanocebe@gmail.com>
2024-04-09 14:46:36 +0200

    katalogoa

--- src/views/supplier/_parts/head.php
+++ src/views/supplier/_parts/head.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/Talde5_holanda/public/Argazkiak/logo.png">
     <link rel="stylesheet" href="/Talde5_holanda/src/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <script src="https://kit.fontawesome.com/7f605dc8fe.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- src/views/supplier/catalogo.php
+++ src/views/supplier/catalogo.php
@@ -4,6 +4,7 @@ require_once ($_SERVER["DOCUMENT_ROOT"] . "/Talde5_holanda/src/views/supplier/_p
 ?>
 
 <link rel="stylesheet" href="/Talde5_holanda/src/css/main.css">
+<link rel="stylesheet" href="/Talde5_holanda/src/css/categoria.css">
 <title>Fiets.Huur </title>
 </head>
 <?php
@@ -46,6 +47,193 @@ if (isset ($_SESSION['nombre_usuario']) && isset ($_SESSION['apellido_usuario'])
             <div class="icon3"><i class="fa-regular fa-user"></i></div>
             <div class="icon4"><i class="fa-solid fa-gear"></i></div>
         </div>
+        </div>
+        <section class="latest top">
+        <div class="scontainer">
+        <div class="heading">
+            <h1>Latest Popular Bike</h1>
+            <div class="lines flex1">
+            <button>50% OFF</button>
+            </div>
+        </div>
+
+        <div class="content grid top">
+            <div class="box">
+            <div class="img">
+                <img src="../../../public/Argazkiak/bicicleta-electrica-removebg-preview.png" width="300">
+                <div class="flex1">
+                <label>50%</label>
+                <i class="fas fa-heart"></i>
+                </div>
+            </div>
+
+            <div class="detalis">
+                <h3>Electric bike</h3>
+                <p>(Comfortable and a very efficient autonomy)</p>
+                <h2>$699 <span>/hour</span> </h2>
+                <button>Rent now</button>
+            </div>
+            </div>
+            <div class="box">
+            <div class="img">
+                <img src="../../../public/Argazkiak/bici-de-ciudad-removebg-preview.png" width="300">
+                <div class="flex1">
+                <label>50%</label>
+                <i class="fas fa-heart"></i>
+                </div>
+            </div>
+
+            <div class="detalis">
+                <h3>Aerion Carrbo Helmet</h3>
+                <p>(Fashion , Twin Disc)</p>
+                <h2>$699 <span>/hour</span> </h2>
+                <button>Rent now</button>
+            </div>
+            </div>
+            <div class="box">
+            <div class="img">
+                <img src="../../../public/Argazkiak/bici-de-carretera-removebg-preview.png" width="300">
+                <div class="flex1">
+                <label>50%</label>
+                <i class="fas fa-heart"></i>
+                </div>
+            </div>
+
+            <div class="detalis">
+                <h3>Aerion Carrbo Helmet</h3>
+                <p>(Fashion , Twin Disc)</p>
+                <h2>$699 <span>/hour</span> </h2>
+                <button>Rent now</button>
+            </div>
+            </div>
+            <div class="box">
+            <div class="img">
+                <img src="../../../public/Argazkiak/bici-electrica-ciudad-removebg-preview.png" width="300">
+                <div class="flex1">
+                <label>50%</label>
+                <i class="fas fa-heart"></i>
+                </div>
+            </div>
+
+            <div class="detalis">
+                <h3>Aerion Carrbo Helmet</h3>
+                <p>(Fashion , Twin Disc)</p>
+                <h2>$699 <span>/hour</span> </h2>
+                <button>Rent now</button>
+            </div>
+            </div>
+            <div class="box">
+            <div class="img">
+                <img src="../../../public/Argazkiak/bicicleta-montaña-removebg-preview.png" width="300">
+                <div class="flex1">
+                <label>50%</label>
+                <i class="fas fa-heart"></i>
+                </div>
+            </div>
+
+            <div class="detalis">
+                <h3>Aerion Carrbo Helmet</h3>
+                <p>(Fashion , Twin Disc)</p>
+                <h2>$699 <span>/hour</span> </h2>
+                <button>Rent now</button>
+            </div>
+            </div>
+            <div class="box">
+            <div class="img">
+                <img src="../../../public/Argazkiak/bmx-removebg-preview.png" width="300">
+                <div class="flex1">
+                <label>50%</label>
+                <i class="fas fa-heart"></i>
+                </div>
+            </div>
+
+            <div class="detalis">
+                <h3>Aerion Carrbo Helmet</h3>
+                <p>(Fashion , Twin Disc)</p>
+                <h2>$699 <span>/hour</span> </h2>
+                <button>Rent now</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="slider top">
+    <div class="scontainer">
+      <div class="owl-carousel owl-theme">
+        <div class="item flex">
+          <div class="left">
+            <p>(Fashion , Twin Disc)</p>
+            <h1>KTM 125 DUKE</h1>
+            <span></span>
+            <ul>
+              <li>- Engine Cc : 370.0cc</li>
+              <li>- Max Power : 14.5 bhp @ 9250 rpm</li>
+              <li>- Tank Capacity : 10.2 L</li>
+              <li>- Wheelbase : 1366 mm</li>
+              <li>- Cooling : Liquid Cooled</li>
+              <li>- Max Torque : 12 Nm @ 8000 rpm</li>
+            </ul>
+            <h2>Price : $1999.99<span>/hour</span></h2>
+            <button class="btn2">Buy Now</button>
+          </div>
+          <div class="right">
+            <div class="ani_image">
+              <img src="image/bike2.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="item flex">
+          <div class="left">
+            <p>(Fashion , Twin Disc)</p>
+            <h1>KTM 125 DUKE</h1>
+            <span></span>
+            <ul>
+              <li>- Engine Cc : 370.0cc</li>
+              <li>- Max Power : 14.5 bhp @ 9250 rpm</li>
+              <li>- Tank Capacity : 10.2 L</li>
+              <li>- Wheelbase : 1366 mm</li>
+              <li>- Cooling : Liquid Cooled</li>
+              <li>- Max Torque : 12 Nm @ 8000 rpm</li>
+            </ul>
+            <h2>Price : $1999.99<span>/hour</span></h2>
+            <button class="btn2">Buy Now</button>
+          </div>
+          <div class="right">
+            <div class="ani_image">
+              <img src="image/bike2.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  </section>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Owl Carousel JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <!-- Script para inicializar Owl Carousel -->
+    <script>
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true, // Habilitar navegación
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:3
+                }
+            },
+            navText: [
+                "<i class='fas fa-chevron-left'></i>",
+                "<i class='fas fa-chevron-right'></i>"
+            ]
+        });
+    });
+    </script>
 </body>
